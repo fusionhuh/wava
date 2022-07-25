2fba10ba20bd29653df423e97592b2e244eddb1b
Josh <joshk559@gmail.com>
2022-07-25 15:21:37 -0500

    Change too much to put in one commit

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 CC = gcc
 CFLAGS = -std=c++14 -pthread `pkg-config --cflags libpulse-simple`
 LIBS = -lm -lstdc++ -lfftw3 `pkg-config --libs libpulse-simple`
-DEPS = graphics.h
-OBJ = wava.o graphics.o
+DEPS = output/cli.h output/graphics.h input/common.h input/pulse.h transform/wavatransform.h
+OBJ = wava.o output/cli.o output/graphics.o input/pulse.o input/common.o transform/wavatransform.o
 
 %.o: %.c $(DEPS)
 	$(CC) -c -o $@ $<
- 
+
+%.o: %.cpp $(DEPS)
+	$(CC) -c -o $@ $<
+
 wava: $(OBJ)
 	$(CC) $(CFLAGS) -o $@ $^ $(LIBS)
 
--- wava.cpp
+++ wava.cpp
@@ -15,295 +15,94 @@
 
 #include <fftw3.h>
 
-#include "graphics.h"
+#include "input/common.h"
+#include "input/pulse.h"
 
-#define SAMPLE_RATE 44100
-#define BUFFER_SIZE 8192 // FFT works better with powers of two
+#include "transform/wavatransform.h"
 
-double fftw_data[BUFFER_SIZE] = {0};
-double fftw_in[BUFFER_SIZE] = {0};             //
-double fftw_backup_buffer[BUFFER_SIZE] = {0}; // Allocating these arrays on data section to avoid excessive and unnecessary
-double amplitude_data[BUFFER_SIZE] = {0};   // malloc calls in pa_stream_callback
-int curr_fftw_data_size = 0;
-int curr_backup_buffer_size = 0;
-bool should_read_from_fftw_data = false;
+#include "output/graphics.h"
+#include "output/cli.h"
 
-std::mutex mtx;
+// PRIORITIES AND NOTES AT THE MOMENT:
 
-struct fftw_analysis {
-  float average_bass_value = 0;
-};
-
-fftw_analysis analyze_fftw_data() {
-  if (curr_fftw_data_size <= 0) { return (fftw_analysis) {0}; }
-
-  float highest_bass_frequency = 200;
-  int index = (int) ((highest_bass_frequency/(SAMPLE_RATE/2)) * curr_fftw_data_size);
-
-  float average_value = 0.00001;
-  if (index > 0) { // implement more elegant solution for checking later
-    for (int i = 0; i < index; ++i) {
-      //printf ("inside loop, data value is: %f\n", fftw_data[i]);
-      if (fftw_data[i] > 0.3) average_value+=fftw_data[i];
-    }
-
-    average_value/=index;
-  }
-
-  for (int i = 0; i < BUFFER_SIZE; i++) fftw_data[i] = 0;
-  //printf ("Average value is: %f", average_value);
-
-  return (fftw_analysis) {average_value};
-}
-
-int access_fftw_buffer(bool write, double* data, int data_length /*only applicable for writes*/, fftw_analysis &analysis) {
-  if (!(mtx.try_lock())) { // accessing failed, locked by other thread
-    mtx.unlock();
-    //printf ("locked\n");
-    return -1; // failure code
-  } 
-
-  else {
-    if(write) // pa thread is trying to write
-    { 
-      for (int i = 0; i < BUFFER_SIZE; i++) {
-        fftw_data[i] = data[i];
-      }
-      should_read_from_fftw_data = true;
-      mtx.unlock();
-      return 0;
-    }
-    else if (should_read_from_fftw_data) { // draw thread is trying to read, need to check if new audio data has been processed
-      analysis = analyze_fftw_data();
-      should_read_from_fftw_data = false;
-      mtx.unlock();
-      return 0;
-    }
-    else {
-      return 0;
-    }
-  }
-}
-
-void pa_stream_notify_cb(pa_stream *stream, void* userdata)
-{
-    const pa_stream_state state = pa_stream_get_state(stream);
-    switch (state) {
-    case PA_STREAM_FAILED:
-	std::cout << "Stream failed\n";
-	break;
-    case PA_STREAM_READY:
-	std::cout << "Stream ready\n";
-	break;
-    default:
-	std::cout << "Stream state: " << state << std::endl;
-    }
-}
-
-void pa_stream_read_cb(pa_stream *stream, const size_t nbytes, void* userdata)
-{
-    // Careful when to pa_stream_peek() and pa_stream_drop()!
-    // c.f. https://www.freedesktop.org/software/pulseaudio/doxygen/stream_8h.html#ac2838c449cde56e169224d7fe3d00824
-    int16_t *data = nullptr;
-    size_t stream_length = 0;
-    if (pa_stream_peek(stream, (const void**)&data, &stream_length) != 0) {
-        std::cerr << "Failed to peek at stream data\n";
-        return;
-    }
-
-    if (data == nullptr && stream_length == 0) {
-	  // No data in the buffer, ignore.
-        return;
-    } else if (data == nullptr && stream_length > 0) {
-        // Hole in the buffer. We must drop it.
-      if (pa_stream_drop(stream) != 0) {
-          std::cerr << "Failed to drop a hole! (Sounds weird, doesn't it?)\n";
-          return;
-      }
-    }
-
-
-    // process data
-    fftw_complex *out;
-    fftw_plan p;
-
-    int space_until_full = BUFFER_SIZE - curr_fftw_data_size;
-
-    if (curr_backup_buffer_size <= space_until_full) {
-      for (int i = 0; i < curr_backup_buffer_size; i++) {
-        fftw_in[curr_fftw_data_size + i] = fftw_backup_buffer[i];
-        space_until_full-=curr_backup_buffer_size;
-        curr_backup_buffer_size = 0;
-      }
-    }
-    else {
-      for (int i = 0; i < space_until_full; i++) {
-        fftw_in[curr_fftw_data_size + i] = fftw_backup_buffer[i];
-        space_until_full = 0;
-        curr_backup_buffer_size-=space_until_full;
-        curr_fftw_data_size = BUFFER_SIZE;
-      }
-    }
-
-    
-
-    int fftw_in_write_size, backup_buffer_write_size;
-
-    if (space_until_full >= stream_length) { // i.e. enough space in fftw_buffer to store new stream data
-      backup_buffer_write_size = 0;
-      fftw_in_write_size = stream_length;
-      curr_fftw_data_size += stream_length;
-    }
-    else { // not enough space in just fftw_buffer
-      fftw_in_write_size = space_until_full;
-      curr_fftw_data_size = BUFFER_SIZE;
-
-      backup_buffer_write_size = stream_length - space_until_full;
-      //printf ("REMAINING SPACE IS: %d\nREQUIRED SPACE IS: %d\n\n\n\n\n", (BUFFER_SIZE - curr_backup_buffer_size), backup_buffer_write_size);
-      if ((BUFFER_SIZE - curr_backup_buffer_size) - backup_buffer_write_size >= 0) {
-        curr_backup_buffer_size += backup_buffer_write_size;
-      }
-      else { // backup buffer overflow; need to dump for now, but can prob develop more elegant solution
-        curr_backup_buffer_size = 0;
-        backup_buffer_write_size = 0;
-      }
-    }
-
-    for (int i = 0; i < fftw_in_write_size; i++) {
-      fftw_in[curr_fftw_data_size + i] = data[i];
-    }
-
-    for (int i = 0; i < backup_buffer_write_size; i++) {
-      fftw_backup_buffer[curr_backup_buffer_size + i] = data[space_until_full + i];
-    }
-
-    out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * ((int )(BUFFER_SIZE/2 + 1)) );    
-    p = fftw_plan_dft_r2c_1d(BUFFER_SIZE, fftw_in, out, FFTW_MEASURE);
-
-    fftw_execute(p);
-    // process frequency data for this "frame"
-
-    double largest_amplitude = .01;
-
-    for (int i = 0; i < (BUFFER_SIZE/2) + 1; i++) {
-      double cos_amp = out[i][0], sin_amp = out[i][1];
-
-      amplitude_data[i] = sqrt(cos_amp*cos_amp + sin_amp*sin_amp);
-
-      if (amplitude_data[i] > largest_amplitude) largest_amplitude = amplitude_data[i];
-    }
-
-    for (int i = 0; i < (BUFFER_SIZE/2) + 1; i++) amplitude_data[i]/=largest_amplitude;
-
-    fftw_analysis shut_the_fuck_up_gcc = (fftw_analysis) {0}; // <- this is HORRENDOUS PLEASE FIX THIS
-    
-    int err;
-    if (curr_fftw_data_size == BUFFER_SIZE) {
-     err = access_fftw_buffer(true, amplitude_data, stream_length, shut_the_fuck_up_gcc); // <- this is seriously flawed and needs work
-     curr_fftw_data_size = 0;
-    }
-
-    fftw_destroy_plan(p);
-    fftw_free(out);
-
-    if (pa_stream_drop(stream) != 0) {
-	    std::cerr << "Failed to drop data after peeking.\n";
-    }
-}
-
-void pa_server_info_cb(pa_context *ctx, const pa_server_info *info, void* userdata)
-{
-    std::cout << "Default sink: " << info->default_sink_name << std::endl;
-
-    pa_sample_spec spec;
-    spec.format = PA_SAMPLE_S16LE;
-    spec.rate = 44100;
-    spec.channels = 1;
-    // Use pa_stream_new_with_proplist instead?
-    pa_stream *stream = pa_stream_new(ctx, "output monitor", &spec, nullptr);
-
-    pa_stream_set_state_callback(stream, &pa_stream_notify_cb, userdata);
-    pa_stream_set_read_callback(stream, &pa_stream_read_cb, userdata);
-
-    std::string monitor_name(info->default_sink_name);
-    monitor_name += ".monitor";
-    if (pa_stream_connect_record(stream, monitor_name.c_str(), nullptr, PA_STREAM_NOFLAGS) != 0) {
-      std::cerr << "connection fail\n";
-      return;
-    }
-
-    std::cout << "Connected to " << monitor_name << std::endl;
-}
-
-void pa_context_notify_cb(pa_context *ctx, void* userdata)
-{
-    const pa_context_state state = pa_context_get_state(ctx);
-    switch (state) {
-    case PA_CONTEXT_READY:
-	std::cout << "Context ready\n";
-	pa_context_get_server_info(ctx, &pa_server_info_cb, userdata);
-	break;
-    case PA_CONTEXT_FAILED:
-	std::cout << "Context failed\n";
-	break;
-    default:
-	std::cout << "Context state: " << state << std::endl;
-    }
-}
-
-int draw () {
-  float A = 0, B = 0;
-  float speed = 0.01;
-  int time = 0;
-
-  //main drawing loop
-
-  fftw_analysis analysis;
-
-  while (true)
-  {
-    if (time == (int) 1/speed) { A = 0; B = 0; time = 0; }
-    
-    int err = access_fftw_buffer(false, NULL, 0, analysis);
-    
-    if (err == 0) {
-      printf ("Average bass value: %f\n", analysis.average_bass_value/2);
-    }
-
-    //render_frame (A, B, analysis.average_bass_value/2); // stuttering can come from average value being NaN
-    usleep (10000);
-    A += 3.14 * 2 * speed; 
-    B += 3.14 * 2 * speed;
-    time += 1;
-  }
-}
+// Might want to look into monstercat filter?
 
 int main(int argc, char **argv)
 {
+	srand(time(0)); // set RNG seed
+	int time = 0;
 
-  void* userdata;
+	// main loop	
+	while (true) {
+		printf ("wava.cpp flag 1\n");
+		// LOAD CONFIG VALUES
+		int freq_bands = 2;
+		std::vector<Shape*> shapes = generate_rand_shapes(2, 0, 0);
+		wava_screen::light.normalize();
+		wava_screen::light_smoothness = 5;
+		// config values end
 
-  pa_mainloop *loop = pa_mainloop_new();
-  pa_mainloop_api *api = pa_mainloop_get_api(loop);
-  pa_context *ctx = pa_context_new(api, "padump");
-  pa_context_set_state_callback(ctx, &pa_context_notify_cb, userdata);
-  if (pa_context_connect(ctx, nullptr, PA_CONTEXT_NOFLAGS, nullptr) < 0) {
-    std::cerr << "PA connection failed.\n";
-    return -1;
-  }
+		struct audio_data audio;
+		memset(&audio, 0, sizeof(audio));
 
-  // pa_stream_disconnect(..)
-  std::thread t1(draw);
-  std::thread t2(pa_mainloop_run, loop, nullptr);
+		//audio.source = "temp" // temporary solution
 
-  t1.join();
-  //t2.join();
-  // gotta fix the segfault that occurs here
+		audio.format = -1; 
+		audio.rate = 0;
+		audio.samples_counter = 0;
+		audio.channels = 2;
 
-  //pa_context_disconnect(ctx);
-  //pa_mainloop_free(loop);
+		audio.input_buffer_size = 512 * audio.channels;
+		audio.wava_buffer_size = audio.input_buffer_size * 8;
 
-  return 0;
+		audio.wava_in = new double[audio.wava_buffer_size];
+
+		audio.terminate = 0;
+
+		pthread_t p_thread;
+
+		// here is where we would have a switch for audio input
+		// case INPUT_PULSE:
+
+		//if (strcmp(audio.source, "auto") == 0) {
+		get_pulse_default_sink((void*) &audio);
+		//}
+		
+		// starting pulsemusic listener
+		int thr_id = pthread_create(&p_thread, NULL, input_pulse, (void*) &audio);
+		audio.rate = 44100;
+		//break
+
+		while (true) { // while (!reloadConf) 
+			int curr_screen_x = 50; 
+			int curr_screen_y = 50; // could be changed in next loop
+
+			struct wava_plan* plan = wava_init(10, 44100, 2, 0.77, 50, 10000);
+			double* wava_out = new double[plan->frequency_bands]();
+
+			wava_screen* screen = new wava_screen(curr_screen_x, curr_screen_y);
+
+			while (true) { // while (!resizeTerminal)
+				pthread_mutex_lock(&audio.lock);
+				wava_execute(audio.wava_in, audio.samples_counter, wava_out, plan);
+				if (audio.samples_counter > 0) audio.samples_counter = 0;
+				pthread_mutex_unlock(&audio.lock);
+				render_cli_frame(shapes, screen, wava_out);
+				usleep(5000);
+				time+=1;        
+			}	
+
+		    wava_destroy(plan);
+			free(plan);
+
+			delete [] wava_out; // not sure if this belongs in this scope
+
+			screen->destroy();
+			delete screen;
+		}
+		for (int i = 0; i < shapes.size(); i++) delete shapes[i];
+	}
+	return 0;
 }
 
 
